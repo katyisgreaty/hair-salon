e7d118465c44236f926f2f13f675167ae6f0ebc4
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-02-24 10:27:23 -0800

    pass tenth spec test which checks if client has been saved in db or not

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -77,6 +77,38 @@ namespace HairSalon
             }
         }
 
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO clients (name, stylist_id) OUTPUT INSERTED.id VALUES (@ClientName, @ClientStylistId);", conn);
+
+            SqlParameter nameParameter = new SqlParameter("@ClientName", this.GetName());
+
+            SqlParameter stylistIdParameter = new SqlParameter();
+            stylistIdParameter.ParameterName = "@ClientStylistId";
+            stylistIdParameter.Value = this.GetStylistId();
+
+            cmd.Parameters.Add(nameParameter);
+            cmd.Parameters.Add(stylistIdParameter);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._id = rdr.GetInt32(0);
+            }
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+
 
 
         public static void DeleteAll()
