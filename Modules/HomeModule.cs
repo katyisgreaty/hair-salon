983a52565e14e8ff2c5741e21eda78c1e901a7bf
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-02-24 11:57:57 -0800

    complete views files and add home module code which passes all tests

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,7 +13,66 @@ namespace HairSalon
                 return View["index.cshtml", AllStylists];
             };
 
+            Get["/clients"] = _ => {
+                List<Client> AllClients = Client.GetAll();
+                return View["clients.cshtml", AllClients];
+            };
 
+            Get["/stylists"] = _ => {
+                List<Stylist> AllStylists = Stylist.GetAll();
+                return View["stylists.cshtml", AllStylists];
+            };
+
+            Get["/stylists/new"] = _ => {
+                return View["stylist_form.cshtml"];
+            };
+            Post["/stylists/new"] = _ => {
+                Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-specialty"], Request.Form["stylist-experience"]);
+                newStylist.Save();
+                return View["success.cshtml"];
+            };
+
+            Get["/clients/new"] = _ => {
+                List<Stylist> AllStylists = Stylist.GetAll();
+                return View["client_form.cshtml", AllStylists];
+            };
+
+            Post["/clients/new"] = _ => {
+                Client newClient = new Client(Request.Form["client-name"], Request.Form["stylist-id"]);
+                newClient.Save();
+                return View["success.cshtml"];
+            };
+
+            Get["/stylists/{id}"] = parameters => {
+                Dictionary<string, object> model = new Dictionary<string, object>();
+                var SelectedStylist = Stylist.Find(parameters.id);
+                var StylistClients = SelectedStylist.GetClients();
+                model.Add("stylist", SelectedStylist);
+                model.Add("clients", StylistClients);
+                return View["stylist.cshtml", model];
+            };
+
+            Get["/client/{id}"] = parameters => {
+                Client SelectedClient = Client.Find(parameters.id);
+                return View["client.cshtml", SelectedClient];
+            };
+
+            Get["client/edit/{id}"] = parameters => {
+                Client SelectedClient = Client.Find(parameters.id);
+                return View["client_update_form.cshtml", SelectedClient];
+            };
+
+            Patch["client/edit/{id}"] = parameters => {
+                Client SelectedClient = Client.Find(parameters.id);
+                SelectedClient.UpdateName(Request.Form["client-name"]);
+                return View["client_updated.cshtml", SelectedClient];
+            };
+
+            Delete["client/delete/{id}"] = parameters => {
+                Client SelectedClient = Client.Find(parameters.id);
+                SelectedClient.Delete();
+                return View["client_deleted.cshtml"];
+            };
 
 
 
