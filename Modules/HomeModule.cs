9662f2d8a0d7bad24309f26855f3a8aadab1c90a
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-02-24 11:42:50 -0800

    add views, write code for many cshtml files including index, client, client forms

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace HairSalon
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => {
+                List<Stylist> AllStylists = Stylist.GetAll();
+                return View["index.cshtml", AllStylists];
+            };
+
+
+
+
+
+        }
+    }
+}
